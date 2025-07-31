544980cdfccf767ca7c2824bdec4436b49385635
David Gageot <david.gageot@docker.com>
2025-07-31 07:24:01 +0200

    Allow overiding the auth token
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- pkg/desktop/login.go
+++ pkg/desktop/login.go
@@ -2,6 +2,7 @@ package desktop
 
 import (
 	"context"
+	"os"
 	"sync"
 )
 
@@ -21,6 +22,13 @@ func IsLoggedIn(ctx context.Context) bool {
 }
 
 func GetToken(ctx context.Context) string {
+	// Allow the user to override the token via an environment variable.
+	// This is e.g useful when talking to a gateway on staging.
+	manualToken := os.Getenv("DOCKER_TOKEN")
+	if manualToken != "" {
+		return manualToken
+	}
+
 	var token string
 	_ = ClientBackend.Get(ctx, "/registry/token", &token)
 	return token
