8eedec7899f34db0c020fcaa57b5224dbf1cb72d
David Gageot <david.gageot@docker.com>
2025-08-01 12:48:49 +0200

    Use our own battle tested stdio client
    + Can show the logs of the server
    + Doesn't block when server can't start
    + Doesn't randomly fail to stop
    
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- pkg/tools/mcp/client.go
+++ pkg/tools/mcp/client.go
@@ -7,15 +7,27 @@ import (
 	"log/slog"
 	"slices"
 
-	"github.com/mark3labs/mcp-go/client"
 	"github.com/mark3labs/mcp-go/mcp"
 
 	"github.com/docker/cagent/pkg/tools"
 )
 
+type mcpClient interface {
+	Start(ctx context.Context) error
+	Initialize(ctx context.Context, request mcp.InitializeRequest) (*mcp.InitializeResult, error)
+	ListTools(ctx context.Context, request mcp.ListToolsRequest) (*mcp.ListToolsResult, error)
+	ListPrompts(ctx context.Context, request mcp.ListPromptsRequest) (*mcp.ListPromptsResult, error)
+	ListResources(ctx context.Context, request mcp.ListResourcesRequest) (*mcp.ListResourcesResult, error)
+	ListResourceTemplates(ctx context.Context, request mcp.ListResourceTemplatesRequest) (*mcp.ListResourceTemplatesResult, error)
+	CallTool(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error)
+	GetPrompt(ctx context.Context, request mcp.GetPromptRequest) (*mcp.GetPromptResult, error)
+	ReadResource(ctx context.Context, request mcp.ReadResourceRequest) (*mcp.ReadResourceResult, error)
+	Close() error
+}
+
 // Client implements an MCP client for interacting with MCP servers
 type Client struct {
-	client  *client.Client
+	client  mcpClient
 	tools   []tools.Tool
 	logger  *slog.Logger
 	logType string
--- pkg/tools/mcp/stdio.go
+++ pkg/tools/mcp/stdio.go
@@ -1,22 +1,28 @@
 package mcp
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
+	"os/exec"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"syscall"
 
-	"github.com/mark3labs/mcp-go/client"
+	"github.com/mark3labs/mcp-go/mcp"
 )
 
 // NewStdioClient creates a new MCP client that can start an stdio MCP server
 func NewStdioClient(ctx context.Context, command string, args, env []string, logger *slog.Logger) (*Client, error) {
 	logger.Debug("Creating stdio MCP client", "command", command, "args", args)
 
-	c, err := client.NewStdioMCPClient(command, env, args...)
-	if err != nil {
-		logger.Error("Failed to create stdio MCP client", "error", err)
-		return nil, fmt.Errorf("failed to create stdio MCP client: %w", err)
-	}
+	c := newStdioCmdClient(command, env, args, logger)
 
 	logger.Debug("Created stdio MCP client successfully")
 	return &Client{
@@ -25,4 +31,279 @@ func NewStdioClient(ctx context.Context, command string, args, env []string, log
 		logType: "command",
 		logId:   command,
 	}, nil
+}
+
+type stdioMCPClient struct {
+	command string
+	env     []string
+	args    []string
+	logger  *slog.Logger
+
+	stdin       io.WriteCloser
+	requestID   atomic.Int64
+	responses   sync.Map
+	close       func() error
+	initialized atomic.Bool
+}
+
+type RPCResponse struct {
+	Error    *string
+	Response *json.RawMessage
+}
+
+type BaseMessage struct {
+	JSONRPC string          `json:"jsonrpc"`
+	ID      *int64          `json:"id,omitempty"`
+	Method  string          `json:"method,omitempty"`
+	Result  json.RawMessage `json:"result,omitempty"`
+	Error   *struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"error,omitempty"`
+}
+
+func newStdioCmdClient(command string, env, args []string, logger *slog.Logger) *stdioMCPClient {
+	return &stdioMCPClient{
+		command: command,
+		env:     env,
+		args:    args,
+		logger:  logger,
+	}
+}
+
+func (c *stdioMCPClient) Start(ctx context.Context) error {
+	return nil
+}
+
+func (c *stdioMCPClient) Initialize(ctx context.Context, request mcp.InitializeRequest) (*mcp.InitializeResult, error) { //nolint:gocritic
+	if c.initialized.Load() {
+		return nil, errors.New("client already initialized")
+	}
+
+	ctxCmd, cancel := context.WithCancel(context.WithoutCancel(ctx))
+	cmd := exec.CommandContext(ctxCmd, c.command, c.args...)
+	cmd.Env = c.env
+	cmd.Cancel = func() error {
+		if runtime.GOOS == "windows" {
+			return cmd.Process.Kill()
+		}
+		return cmd.Process.Signal(syscall.SIGTERM)
+	}
+
+	var stderr bytes.Buffer
+	if c.logger.Handler().Enabled(ctx, slog.LevelDebug) {
+		logLogger := slog.NewLogLogger(c.logger.Handler(), slog.LevelDebug)
+		cmd.Stderr = io.MultiWriter(&stderr, logLogger.Writer())
+	} else {
+		cmd.Stderr = io.MultiWriter(&stderr)
+	}
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		cancel()
+		return nil, fmt.Errorf("failed to create stdin pipe: %w", err)
+	}
+	c.stdin = stdin
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		cancel()
+		return nil, fmt.Errorf("failed to create stdout pipe: %w", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		cancel()
+		return nil, fmt.Errorf("failed to start command: %w", err)
+	}
+
+	c.close = func() error {
+		cancel()
+		return nil
+	}
+	go func() {
+		_ = cmd.Wait()
+		cancel()
+	}()
+	go func() {
+		_ = c.readResponses(bufio.NewReader(stdout))
+	}()
+
+	var result mcp.InitializeResult
+	errCh := make(chan error)
+	go func() {
+		errCh <- func() error {
+			params := struct {
+				ProtocolVersion string                 `json:"protocolVersion"`
+				ClientInfo      mcp.Implementation     `json:"clientInfo"`
+				Capabilities    mcp.ClientCapabilities `json:"capabilities"`
+			}{
+				ProtocolVersion: request.Params.ProtocolVersion,
+				ClientInfo:      request.Params.ClientInfo,
+				Capabilities:    request.Params.Capabilities,
+			}
+
+			if err := c.request(ctxCmd, "initialize", params, &result); err != nil {
+				return err
+			}
+
+			encoder := json.NewEncoder(stdin)
+			if err := encoder.Encode(mcp.JSONRPCNotification{
+				JSONRPC: mcp.JSONRPC_VERSION,
+				Notification: mcp.Notification{
+					Method: "notifications/initialized",
+				},
+			}); err != nil {
+				return fmt.Errorf("failed to marshal initialized notification: %w", err)
+			}
+
+			c.initialized.Store(true)
+			return nil
+		}()
+	}()
+
+	select {
+	case <-ctxCmd.Done():
+		return nil, errors.New(stderr.String())
+	case <-ctx.Done():
+		cancel() // need to also cancel command if timed out or cancelled from parent
+		return nil, ctx.Err()
+	case err := <-errCh:
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &result, nil
+}
+
+func (c *stdioMCPClient) Close() error {
+	return c.close()
+}
+
+func (c *stdioMCPClient) readResponses(stdout *bufio.Reader) error {
+	for {
+		buf, err := stdout.ReadBytes('\n')
+		if err != nil {
+			return err
+		}
+
+		var baseMessage BaseMessage
+		if err := json.Unmarshal(buf, &baseMessage); err != nil {
+			continue
+		}
+
+		if baseMessage.ID == nil {
+			continue
+		}
+
+		if ch, ok := c.responses.LoadAndDelete(*baseMessage.ID); ok {
+			responseChan := ch.(chan RPCResponse)
+
+			if baseMessage.Error != nil {
+				responseChan <- RPCResponse{
+					Error: &baseMessage.Error.Message,
+				}
+			} else {
+				responseChan <- RPCResponse{
+					Response: &baseMessage.Result,
+				}
+			}
+		}
+	}
+}
+
+func (c *stdioMCPClient) sendRequest(ctx context.Context, method string, params any) (*json.RawMessage, error) {
+	id := c.requestID.Add(1)
+	responseChan := make(chan RPCResponse, 1)
+	c.responses.Store(id, responseChan)
+
+	encoder := json.NewEncoder(c.stdin)
+	if err := encoder.Encode(mcp.JSONRPCRequest{
+		JSONRPC: mcp.JSONRPC_VERSION,
+		ID:      mcp.NewRequestId(id),
+		Request: mcp.Request{
+			Method: method,
+		},
+		Params: params,
+	}); err != nil {
+		return nil, fmt.Errorf("failed to encode request: %w", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case response := <-responseChan:
+		if response.Error != nil {
+			return nil, errors.New(*response.Error)
+		}
+		return response.Response, nil
+	}
+}
+
+func (c *stdioMCPClient) request(ctx context.Context, method string, params, v any) error {
+	response, err := c.sendRequest(ctx, method, params)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(*response, &v)
+}
+
+func (c *stdioMCPClient) ListTools(ctx context.Context, request mcp.ListToolsRequest) (*mcp.ListToolsResult, error) {
+	var result mcp.ListToolsResult
+	if err := c.request(ctx, "tools/list", request, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *stdioMCPClient) ListPrompts(ctx context.Context, request mcp.ListPromptsRequest) (*mcp.ListPromptsResult, error) {
+	var result mcp.ListPromptsResult
+	if err := c.request(ctx, "prompts/list", request, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *stdioMCPClient) ListResources(ctx context.Context, request mcp.ListResourcesRequest) (*mcp.ListResourcesResult, error) {
+	var result mcp.ListResourcesResult
+	if err := c.request(ctx, "resources/list", request, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *stdioMCPClient) ListResourceTemplates(ctx context.Context, request mcp.ListResourceTemplatesRequest) (*mcp.ListResourceTemplatesResult, error) {
+	var result mcp.ListResourceTemplatesResult
+	if err := c.request(ctx, "resources/templates/list", request, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *stdioMCPClient) CallTool(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	response, err := c.sendRequest(ctx, "tools/call", request.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	return mcp.ParseCallToolResult(response)
+}
+
+func (c *stdioMCPClient) GetPrompt(ctx context.Context, request mcp.GetPromptRequest) (*mcp.GetPromptResult, error) {
+	response, err := c.sendRequest(ctx, "prompts/get", request.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	return mcp.ParseGetPromptResult(response)
+}
+
+func (c *stdioMCPClient) ReadResource(ctx context.Context, request mcp.ReadResourceRequest) (*mcp.ReadResourceResult, error) {
+	response, err := c.sendRequest(ctx, "resources/read", request.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	return mcp.ParseReadResourceResult(response)
 }
