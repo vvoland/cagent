fd449b2b3eff7a85327c4421b04622f7327b5e0b
David Gageot <david.gageot@docker.com>
2025-10-16 20:58:08 +0200

    Remove redundant Output: prefix
    Fix #530
    
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- pkg/tools/builtin/shell.go
+++ pkg/tools/builtin/shell.go
@@ -103,8 +103,14 @@ func (h *shellHandler) RunShell(ctx context.Context, toolCall tools.ToolCall) (*
 		}, nil
 	}
 
+	if len(output) == 0 {
+		return &tools.ToolCallResult{
+			Output: "<no output>",
+		}, nil
+	}
+
 	return &tools.ToolCallResult{
-		Output: fmt.Sprintf("Output: %s", output),
+		Output: output,
 	}, nil
 }
 
