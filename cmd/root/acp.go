9e3ccd48cbbf30dde481d9ea6cf46c76dd2d8105
Djordje Lukic <djordje.lukic@docker.com>
2025-10-23 11:07:17 +0200

    Implement the ACP protocol
    With the new `cagent acp <agent.yaml` command
    
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

---
+++ cmd/root/acp.go
@@ -0,0 +1,50 @@
+package root
+
+import (
+	"log/slog"
+	"os"
+
+	acpsdk "github.com/coder/acp-go-sdk"
+	"github.com/spf13/cobra"
+
+	"github.com/docker/cagent/pkg/acp"
+	"github.com/docker/cagent/pkg/telemetry"
+)
+
+// NewACPCmd creates a new acp command
+func NewACPCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "acp <agent-file>",
+		Short: "Start an ACP (Agent Client Protocol) server",
+		Long:  `Start an ACP server that exposes the agent via the Agent Client Protocol`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			telemetry.TrackCommand("acp", args)
+			return runACP(cmd, args)
+		},
+	}
+
+	addGatewayFlags(cmd)
+	addRuntimeConfigFlags(cmd)
+
+	return cmd
+}
+
+func runACP(cmd *cobra.Command, args []string) error {
+	ctx := cmd.Context()
+	agentFilename := args[0]
+
+	slog.Debug("Starting ACP server", "agent_file", agentFilename, "debug_mode", debugMode)
+
+	acpAgent := acp.NewAgent(agentFilename, runConfig)
+	conn := acpsdk.NewAgentSideConnection(acpAgent, os.Stdout, os.Stdin)
+	conn.SetLogger(slog.Default())
+	acpAgent.SetAgentConnection(conn)
+	defer acpAgent.Stop(ctx)
+
+	slog.Debug("acp started, waiting for conn")
+
+	<-conn.Done()
+
+	return nil
+}
