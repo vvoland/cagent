ef178b1c03877c578a0ea18d63b5b598d2fda91b
David Gageot <david.gageot@docker.com>
2025-10-28 12:20:31 +0100

    Set a consistent user-agent
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- pkg/httpclient/client.go
+++ pkg/httpclient/client.go
@@ -1,7 +1,29 @@
 package httpclient
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+
+	"github.com/docker/cagent/pkg/version"
+)
+
+type userAgentTransport struct {
+	agent string
+	rt    http.RoundTripper
+}
+
+func (u *userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	r2 := req.Clone(req.Context())
+	r2.Header.Set("User-Agent", u.agent)
+	return u.rt.RoundTrip(r2)
+}
 
 func NewHttpClient() *http.Client {
-	return &http.Client{}
+	return &http.Client{
+		Transport: &userAgentTransport{
+			agent: fmt.Sprintf("Cagent/%s (%s; %s)", version.Version, runtime.GOOS, runtime.GOARCH),
+			rt:    http.DefaultTransport,
+		},
+	}
 }
