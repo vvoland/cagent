e9637f6850b38119a4e3647f403d9f27dfdd023e
Djordje Lukic <djordje.lukic@docker.com>
2025-11-02 22:07:52 +0100

    Just call the chatPage directly
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- pkg/tui/tui.go
+++ pkg/tui/tui.go
@@ -245,18 +245,8 @@ func (a *appModel) handleWindowResize(width, height int) tea.Cmd {
 		cmds = append(cmds, cmd)
 	}
 
-	// Update chat page
-	if sizable, ok := a.chatPage.(interface{ SetSize(int, int) tea.Cmd }); ok {
-		cmd := sizable.SetSize(a.width, a.height)
-		cmds = append(cmds, cmd)
-	} else {
-		// Fallback: send window size message
-		updated, cmd := a.chatPage.Update(tea.WindowSizeMsg{Width: a.width, Height: a.height})
-		a.chatPage = updated.(chat.Page)
-		if cmd != nil {
-			cmds = append(cmds, cmd)
-		}
-	}
+	cmd = a.chatPage.SetSize(a.width, a.height)
+	cmds = append(cmds, cmd)
 
 	// Update status bar width
 	a.statusBar.SetWidth(a.width)
