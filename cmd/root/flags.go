45ac85f5255b0cca32de432bb84c2e6ac83c803b
Djordje Lukic <djordje.lukic@docker.com>
2025-11-13 12:07:00 +0100

    Remove the redirectUri flag
    It was not really used and only confused me
    
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- cmd/root/flags.go
+++ cmd/root/flags.go
@@ -9,6 +9,5 @@ import (
 func addRuntimeConfigFlags(cmd *cobra.Command, runConfig *config.RuntimeConfig) {
 	addGatewayFlags(cmd, runConfig)
 	cmd.PersistentFlags().StringSliceVar(&runConfig.EnvFiles, "env-from-file", nil, "Set environment variables from file")
-	cmd.PersistentFlags().StringVar(&runConfig.RedirectURI, "redirect-uri", "http://localhost:8083/oauth-callback", "Set the redirect URI for OAuth2 flows")
 	cmd.PersistentFlags().BoolVar(&runConfig.GlobalCodeMode, "code-mode-tools", false, "Provide a single tool to call other tools via Javascript")
 }
