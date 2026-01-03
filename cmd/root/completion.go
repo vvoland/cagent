8ff1bbd779cf19f5e9516d1139477721b35da5cc
David Gageot <david.gageot@docker.com>
2026-01-03 10:18:19 +0100

    Complete commands
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- cmd/root/completion.go
+++ cmd/root/completion.go
@@ -1,24 +1,33 @@
 package root
 
 import (
+	"context"
 	"strings"
 
 	"github.com/docker/cagent/pkg/aliases"
+	"github.com/docker/cagent/pkg/config"
 	"github.com/spf13/cobra"
 )
 
 func completeRunExec(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	if len(args) != 0 {
+	switch len(args) {
+	case 0:
+		return completeAlias(toComplete)
+	case 1:
+		return completeMessage(cmd, args, toComplete)
+	default:
 		return nil, cobra.ShellCompDirectiveNoFileComp
 	}
+}
 
+func completeAlias(toComplete string) ([]string, cobra.ShellCompDirective) {
 	if strings.Contains(toComplete, "/") || strings.HasPrefix(toComplete, ".") {
 		return nil, cobra.ShellCompDirectiveDefault
 	}
 
 	s, err := aliases.Load()
 	if err != nil {
-		// Ignore error and don't provide completions
+		// Ignore error and don't provide alias completions
 		return nil, cobra.ShellCompDirectiveDefault
 	}
 
@@ -30,4 +39,33 @@ func completeRunExec(cmd *cobra.Command, args []string, toComplete string) ([]st
 	}
 
 	return candidates, cobra.ShellCompDirectiveDefault
+}
+
+func completeMessage(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	agentSource, err := config.Resolve(args[0])
+	if err != nil {
+		// Ignore error and don't provide completions
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	cfg, err := config.Load(context.Background(), agentSource)
+	if err != nil {
+		// Ignore error and don't provide completions
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	agent := "root"
+	name, err := cmd.Flags().GetString("agent")
+	if err == nil && name != "" {
+		agent = name
+	}
+
+	var candidates []string
+	for k, v := range cfg.Agents[agent].Commands {
+		if strings.HasPrefix(k, toComplete) {
+			candidates = append(candidates, "/"+k+"\t"+v)
+		}
+	}
+
+	return candidates, cobra.ShellCompDirectiveNoFileComp
 }
